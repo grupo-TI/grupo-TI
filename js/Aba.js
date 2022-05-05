979d1ffe3cd213ed5684658ff52b4f34d2a63564
grupo-TI <104940523+grupo-TI@users.noreply.github.com>
2022-05-04 21:08:15 -0300

    Add files via upload

--- js/Aba.js
+++ js/Aba.js
@@ -1,1 +1,32 @@
+var open = document.querySelector('#open');
+var Aba = document.querySelector('#Aba');
+var html = document.querySelector('html');
 
+var opensty = open.style;
+var htmlsty = html.style;
+var Abastyle = Aba.style;
+
+
+open.addEventListener('click',AbaOn);
+
+function AbaOn(){
+  Abastyle.width = '80%';
+  opensty.transform = 'rotate(45deg)';
+  Abastyle.padding = '65px 0px 0px 10px';
+  htmlsty.overflow = 'hidden';
+  open.addEventListener('click',AbaOff);
+  open.removeEventListener('click',AbaOn);
+  
+}
+
+function AbaOff(){
+  Abastyle.width = '0%';
+  opensty.transform = 'rotate(0deg)';
+  Abastyle.padding = '0px';
+  htmlsty.overflow = 'scroll';
+  open.addEventListener('click',AbaOn);
+  open.removeEventListener('click',AbaOff);
+}
+
+
+//padding: 65px 0px 0px 10px;
